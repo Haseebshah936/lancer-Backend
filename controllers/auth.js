6008acff40aab98dfb0e6c9be69235766c098255
Haseeb shah <haseebshah936@gmail.com>
2022-10-07 13:51:15 +0500

    google auth in process cross-origin issue

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,13 @@
 const mongoose = require('mongoose');
 const User = require("../models/user");
+const {OAuth2Client, UserRefreshClient} = require("google-auth-library")
+const  clientId= "30719619583-j2d2baepb0dkbscqrm3661mb6bomooch.apps.googleusercontent.com"
+const clientSecret= "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"
+const oAuth2Client = new OAuth2Client({
+    clientId: "30719619583-j2d2baepb0dkbscqrm3661mb6bomooch.apps.googleusercontent.com",
+    clientSecret: "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",
+    "postMessage": true,
+})
 const signup = async (req, res) => {
     const {username, email,password} = req.body;
     const user = new User({
@@ -48,8 +56,70 @@ const remove = async (req, res) => {
     }
 }
 
+const googleAuth = async (req, res) => {
+    console.log(req.body);
+    try {
+        const { tokens } = await oAuth2Client.getToken(req.body.code); // exchange code for tokens
+        console.log(tokens);
+        res.status(200).json(tokens);
+        
+    } catch (error) {
+        res.status(500).send(error);
+        console.log(error);
+    }
+  }
+
+ const refreshToken =  async (req, res) => {
+    const user = new UserRefreshClient(
+      clientId,
+      clientSecret,
+      req.body.refreshToken,
+    );
+    const { credentials } = await user.refreshAccessToken(); // optain new tokens
+    res.status(200).json(credentials);
+  }
+  
+// const forgotPassword = async (req, res) => {
+//     try{
+//         const {email} = req.body;
+//         const user = await User.findOne({email});
+//         if(!user) return res.status(403).send("User not found");
+//         const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET);
+//         const link = `http://localhost:3000/reset/${token}`;
+//         const mailOptions = {
+//             from: process.env.EMAIL,
+//             to: email,
+//             subject: "Reset Password",
+//             html: `
+//                 <h2>Please click on the given link to reset your password</h2>
+//                 <p>${link}</p>
+//             `
+//         }
+//         await transporter.sendMail(mailOptions);
+//         res.status(200).send("Password reset link sent to your email");
+//     }
+//     catch(error){
+//         res.status(500).send(error);
+//     }
+// }
+
+// const resetPassword = async (req, res) => {
+//     try{
+//         const {token} = req.params;
+//         const {password} = req.body;
+//         const user = jwt.verify(token, process.env.JWT_SECRET);
+//         const result = await User.findOneAndUpdate({_id: user._id}, {password});
+//         res.status(200).send("Password updated successfully");
+//     }
+//     catch(error){
+//         res.status(500).send(error);
+//     }
+// }
+
 module.exports = {
     signup,
     login,
-    remove
+    remove,
+    googleAuth,
+    refreshToken
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
-const {signup, login, remove} = require('../controllers/auth');
+const {signup, login, remove, googleAuth, refreshToken} = require('../controllers/auth');
 
 router.post('/signup', signup);
 router.post('/login', login);
 router.post('/delete/:id', remove);
+router.post('/google', googleAuth);
+router.post('/google/refreshToken', refreshToken);
 
 module.exports = router;
