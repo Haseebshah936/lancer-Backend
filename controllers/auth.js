7ac3f7d32c28907702c3905f9cee153ac0212dbc
Haseeb shah <haseebshah936@gmail.com>
2022-11-06 20:28:49 +0500

    backend routes for auth done

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,151 +1,227 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const User = require("../models/user");
-const {OAuth2Client, UserRefreshClient} = require("google-auth-library");
+const { OAuth2Client, UserRefreshClient } = require("google-auth-library");
 const config = require("config");
 const clientId = config.get("googleKeys.clientId");
 const clientSecret = config.get("googleKeys.clientSecret");
-const argon2 = require("argon2")
+const argon2 = require("argon2");
 const axios = require("axios");
-const {logger} = require('../utils/logger');
+const { logger } = require("../utils/logger");
+const jwt = require("jsonwebtoken");
 
 const oAuth2Client = new OAuth2Client({
-    clientId,
-    clientSecret,
-    redirectUri: "postmessage",
-})
+  clientId,
+  clientSecret,
+  redirectUri: "postmessage",
+});
 const signup = async (req, res) => {
-    const {username, email,password} = req.body;
-    const encryptedPassword = await argon2.hash(password);
-    const user = new User({
-        name: "test1",
-        email,
-        password: encryptedPassword,
-    })
-    try {
-        const savedUser = await User.findOne({email});
-        if(savedUser) return res.status(403).send("User already exists");
-        const result = await user.save();
-        const {password, ...rest} = result._doc;
-        res.status(201).send({
-            ...rest
-        })
-    } catch (error) {
-        res.status(500).send(error);
-    }
-}
+  const { email, password } = req.body;
+  console.log(req.body);
+  const encryptedPassword = await argon2.hash(password);
+  const user = new User({
+    name: "test1",
+    email,
+    password: encryptedPassword,
+  });
+  try {
+    const savedUser = await User.findOne({ email });
+    if (savedUser) return res.status(403).send("User already exists");
+    const result = await user.save();
+    res.status(201).send({
+      email,
+      password: encryptedPassword,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+};
 
 const login = async (req, res) => {
-    try {
-        const {email} = req.body;
-        const user = await User.findOne({email});
-        if(!user) return res.status(404).send("User not found");
-        if(await argon2.verify(user.password, req.body.password)) return res.status(403).send("Incorrect password");
-        const {password, ...rest} = user._doc;
-        res.status(200).send({
-            ...rest
-        })
-    } catch (error) {
-        res.status(500).send(error);
-    }
-}
+  try {
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).send("User not found");
+    if (await argon2.verify(user.password, req.body.password))
+      return res.status(403).send("Incorrect password");
+    // const { password, ...rest } = user._doc;
+    res.status(200).send({
+      ...user.doc,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+};
 
 const remove = async (req, res) => {
-    try{
-        const user = await User.findById(req.params.id);
-        if(!user) return res.status(403).send("User not found");
-        if(user.password !== req.body.password) return res.status(403).send("Incorrect password");
-        await user.deleteOne();
-        res.status(200).send("User deleted successfully");
-    }
-    catch(error){
-        res.status(500).send(error);
-    }
-}
-
-const googleAuth = async (req, res) => {
-    console.log("Body ",req.body);
-    try {
-        const { tokens } = await oAuth2Client.getToken(req.body.code); // exchange code for tokens
-        const ticket = await oAuth2Client.verifyIdToken({idToken: tokens.id_token, audience: clientId}); // verify token
-        const payload = ticket.getPayload();
-        const user = await User.findOneAndUpdate({email: payload.email}, {googleId: payload.sub}, {new:true});
-        if(!user){
-            const encryptedPassword = await argon2.hash(payload.sub);
-            const newUser = new User({
-                name: payload.name,
-                email: payload.email,
-                password: encryptedPassword,
-                googleId: payload.sub,
-                profilePic: payload.picture,
-                emailVerified: true,
-            })
-            const response = await newUser.save();
-            const {password, ...rest} = response._doc;
-            res.status(201).send({...rest});
-            return
-        }
-        res.status(200).send(user);
-    } catch (error) {
-        res.status(500).send(error);
-        console.log(error);
-    }
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(403).send("User not found");
+    if (user.password !== req.body.password)
+      return res.status(403).send("Incorrect password");
+    await user.deleteOne();
+    res.status(200).send("User deleted successfully");
+  } catch (error) {
+    res.status(500).send(error);
   }
+};
 
-const facebookAuth = async (req, res) => {
-
-    try {
-        const {accessToken, id} = req.body;
-        const { data } = await axios({
-          url: `https://graph.facebook.com/${id}`,
-          method: 'get',
-          params: {
-            fields: ['id', 'email', 'first_name', 'last_name', "picture.type(large)"].join(','),
-            access_token: accessToken,
-          },
-        });
-        logger(data); // { id, email, first_name, last_name }
-        const user = await User.findOneAndUpdate({email: data.email}, {facebookId: data.id},{new:true});
-        if(!user){
-            const encryptedPassword = await argon2.hash(data.id+"kingsman");
-            const newUser = new User({
-                name: data.first_name+" "+data.last_name,
-                email: data.email,
-                password: encryptedPassword,
-                facebookId: data.id,
-                profilePic: data.picture.data.url,
-                emailVerified: true,
-            })
-            const response = await newUser.save();
-            const {password, ...rest} = response._doc;
-            res.status(201).send({...rest});
-            return;
-        }
-        const {password, ...rest} = user._doc;
-        res.status(200).send({...rest});
-        return;
-      }
-      catch (error) {
-        console.error(error);
-        res.status(500).send(error);
-      } 
-}
-
- const refreshToken =  async (req, res) => {
-    const user = new UserRefreshClient(
-      clientId,
-      clientSecret,
-      req.body.refreshToken,
+const googleSignup = async (req, res) => {
+  console.log("Body ", req.body);
+  try {
+    const { tokens } = await oAuth2Client.getToken(req.body.code); // exchange code for tokens
+    const ticket = await oAuth2Client.verifyIdToken({
+      idToken: tokens.id_token,
+      audience: clientId,
+    }); // verify token
+    const payload = ticket.getPayload();
+    const user = await User.findOneAndUpdate(
+      { email: payload.email },
+      { googleId: payload.sub },
+      { new: true }
     );
-    const { credentials } = await user.refreshAccessToken(); // optain new tokens
-    res.status(200).json(credentials);
+    if (!user) {
+      const encryptedPassword = await argon2.hash(payload.sub);
+      const newUser = new User({
+        name: payload.name,
+        email: payload.email,
+        password: encryptedPassword,
+        googleId: payload.sub,
+        profilePic: payload.picture,
+        emailVerified: true,
+      });
+      const response = await newUser.save();
+      const { password, email, ...rest } = response._doc;
+      res.status(201).send({ email, password });
+      return;
+    }
+    res.status(403).send("User already exists");
+  } catch (error) {
+    res.status(500).send(error);
+    console.log(error);
   }
+};
+const facebookSignup = async (req, res) => {
+  try {
+    const { accessToken, id } = req.body;
+    const { data } = await axios({
+      url: `https://graph.facebook.com/${id}`,
+      method: "get",
+      params: {
+        fields: [
+          "id",
+          "email",
+          "first_name",
+          "last_name",
+          "picture.type(large)",
+        ].join(","),
+        access_token: accessToken,
+      },
+    });
+    logger(data); // { id, email, first_name, last_name }
+    const user = await User.findOneAndUpdate(
+      { email: data.email },
+      { facebookId: data.id },
+      { new: true }
+    );
+    if (!user) {
+      const encryptedPassword = await argon2.hash(data.id + "kingsman");
+      const newUser = new User({
+        name: data.first_name + " " + data.last_name,
+        email: data.email,
+        password: encryptedPassword,
+        facebookId: data.id,
+        profilePic: data.picture.data.url,
+        emailVerified: true,
+      });
+      const response = await newUser.save();
+      const { email, password, ...rest } = response._doc;
+      res.status(201).send({ email, password });
+      return;
+    }
+    // const { password, ...rest } = user._doc;
+    // res.status(200).send({ ...user._doc });
+    res.status(403).send("User already exists");
+    return;
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
+};
+const googleLogin = async (req, res) => {
+  console.log("Body ", req.body);
+  try {
+    const { tokens } = await oAuth2Client.getToken(req.body.code); // exchange code for tokens
+    const ticket = await oAuth2Client.verifyIdToken({
+      idToken: tokens.id_token,
+      audience: clientId,
+    }); // verify token
+    const payload = ticket.getPayload();
+    const user = await User.findOneAndUpdate(
+      { email: payload.email },
+      { googleId: payload.sub },
+      { new: true }
+    );
+    if (!user) return res.status(404).send("User not found");
+    // const { password, ...rest } = user._doc;
+    res.status(200).send({ ...user._doc });
+  } catch (error) {
+    res.status(500).send(error);
+    console.log(error);
+  }
+};
+const facebookLogin = async (req, res) => {
+  try {
+    const { accessToken, id } = req.body;
+    const { data } = await axios({
+      url: `https://graph.facebook.com/${id}`,
+      method: "get",
+      params: {
+        fields: [
+          "id",
+          "email",
+          "first_name",
+          "last_name",
+          "picture.type(large)",
+        ].join(","),
+        access_token: accessToken,
+      },
+    });
+    logger(data); // { id, email, first_name, last_name }
+    const user = await User.findOneAndUpdate(
+      { email: data.email },
+      { facebookId: data.id },
+      { new: true }
+    );
+    if (!user) return res.status(404).send("User not found");
+    // const { password, ...rest } = user._doc;
+    res.status(200).send({ ...user._doc });
+    return;
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
+};
 
-  const callback = () => {
-    passport.authenticate("google", { failureRedirect: "http://localhost:3000" }), (req, res) => {
-    // Successful authentication, redirect secrets.
-    res.redirect("http://localhost:3000");};
-  }
-  
+const refreshToken = async (req, res) => {
+  const user = new UserRefreshClient(
+    clientId,
+    clientSecret,
+    req.body.refreshToken
+  );
+  const { credentials } = await user.refreshAccessToken(); // optain new tokens
+  res.status(200).json(credentials);
+};
+
+const callback = () => {
+  passport.authenticate("google", { failureRedirect: "http://localhost:3000" }),
+    (req, res) => {
+      // Successful authentication, redirect secrets.
+      res.redirect("http://localhost:3000");
+    };
+};
+
 // const forgotPassword = async (req, res) => {
 //     try{
 //         const {email} = req.body;
@@ -184,11 +260,13 @@ const facebookAuth = async (req, res) => {
 // }
 
 module.exports = {
-    signup,
-    login,
-    remove,
-    googleAuth,
-    refreshToken,
-    callback,
-    facebookAuth,
-}
+  signup,
+  login,
+  remove,
+  refreshToken,
+  googleSignup,
+  googleLogin,
+  callback,
+  facebookSignup,
+  facebookLogin,
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,24 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const {signup, login, remove, googleAuth, refreshToken, facebookAuth} = require('../controllers/auth');
+const {
+  signup,
+  login,
+  remove,
+  googleSignup,
+  googleLogin,
+  refreshToken,
+  facebookSignup,
+  facebookLogin,
+} = require("../controllers/auth");
 
-router.post('/signup', signup);
-router.post('/login', login);
-router.post('/delete/:id', remove);
-router.post('/google', googleAuth);
-router.post('/google/refreshToken', refreshToken);
-router.post('/google/callback', refreshToken);
-router.post('/facebook/', facebookAuth);
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/delete/:id", remove);
+router.post("/google/signup", googleSignup);
+router.post("/google/login", googleLogin);
+router.post("/google/refreshToken", refreshToken);
+router.post("/google/callback", refreshToken);
+router.post("/facebook/signup", facebookSignup);
+router.post("/facebook/login", facebookLogin);
 
 module.exports = router;
