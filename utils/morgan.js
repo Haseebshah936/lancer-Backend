57dfb921d533b43dfd97e5a32c3ded3fd2795e02
Haseeb shah <haseebshah936@gmail.com>
2022-12-28 03:23:28 +0500

    tiny to dev

--- utils/morgan.js
+++ utils/morgan.js
@@ -3,7 +3,7 @@ const logger = require("../utils/logger").logger;
 
 module.exports = function (app) {
   if (app.get("env") === "development") {
-    app.use(morgan("tiny"));
+    app.use(morgan("dev"));
     logger("Morgan enabled...");
   }
-}
+};
