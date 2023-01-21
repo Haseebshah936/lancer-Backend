5d30ef762f0e72f36a98e69f5d7378fa83d554f9
Haseeb shah <haseebshah936@gmail.com>
2023-01-22 02:10:15 +0500

    testing

--- controllers/message.js
+++ controllers/message.js
@@ -81,31 +81,31 @@ const createMessage = async (req, res) => {
       text,
       uri,
     });
-    axios
-      .post("http://mumerabid.pythonanywhere.com/nlp", {
-        text,
-        id: chatroomId,
-      })
-      .then((res) => {
-        if (
-          res.data.spam_pre === "true" ||
-          res.data.curse_pre === "true" ||
-          res.data.isNumbers === "true" ||
-          res.data.isEmails === "true" ||
-          res.data.isUrls === "true"
-        ) {
-          const newDispute = new CustomerSupport({
-            userId,
-            requestType: "other",
-            disputeReason: text,
-            chatroomId,
-          });
-          newDispute.save();
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    // axios
+    //   .post("http://mumerabid.pythonanywhere.com/nlp", {
+    //     text,
+    //     id: chatroomId,
+    //   })
+    //   .then((res) => {
+    //     if (
+    //       res.data.spam_pre === "true" ||
+    //       res.data.curse_pre === "true" ||
+    //       res.data.isNumbers === "true" ||
+    //       res.data.isEmails === "true" ||
+    //       res.data.isUrls === "true"
+    //     ) {
+    //       const newDispute = new CustomerSupport({
+    //         userId,
+    //         requestType: "other",
+    //         disputeReason: text,
+    //         chatroomId,
+    //       });
+    //       newDispute.save();
+    //     }
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
     chatroom.latestMessage = newMessage._id;
     const participantId = chatroom.participants.filter(
       (participant) => participant.userId.toString() === userId._id
