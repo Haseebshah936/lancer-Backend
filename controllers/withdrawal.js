db6e894e0590d15c804d0a0ca07aea7b7a3c48b5
Haseeb shah <haseebshah936@gmail.com>
2023-01-30 02:37:47 +0500

    some minor adjustment

--- controllers/withdrawal.js
+++ controllers/withdrawal.js
@@ -138,7 +138,7 @@ const createWithdrawl = async (req, res) => {
     user.currentBalance -= amount;
     await user.save();
     const withdrawal = await newWithdrawl.save();
-    res.status(200).json(withdrawal);
+    res.status(200).json(user);
   } catch (error) {
     res.status(500).json(error);
   }
